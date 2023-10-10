f0ec031d300dd931a45c6be91061b9420b04a088
Serpe05 <8293352@itisrossi.vi.it>
2023-10-10 11:40:25 +0200

    base bottoni

--- src/FormPK/CalcolatriceRPN.java
+++ src/FormPK/CalcolatriceRPN.java
@@ -4,6 +4,22 @@ import javax.swing.*;
 
 public class CalcolatriceRPN {
     private JPanel Panel;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton ceButton;
+    private JButton a4Button;
+    private JButton a1Button;
+    private JButton a0Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton button10;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton xButton;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("CalcolatriceRPN");
